f0860d5a6f162330af4d7a4a4a7e48624ac14f81
filip <filip.vujic665@gmail.com>
2020-01-31 15:20:42 +0100

    gotov projekat

--- build/generated-sources/ap-source-output/entity/RecipeTag_.java
+++ build/generated-sources/ap-source-output/entity/RecipeTag_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-30T23:04:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2020-01-31T10:54:42")
 @StaticMetamodel(RecipeTag.class)
 public class RecipeTag_ { 
 
